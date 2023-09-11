667fc4663487cd29ef04747a3593782e08dadf39
SquirrelKiev <laidbackenon@outlook.com>
2023-09-11 20:18:30 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'DAVIGO Map Editor',
+  title: 'DAVIGO Wiki',
   //favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
