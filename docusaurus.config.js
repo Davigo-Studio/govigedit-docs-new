feeadf84cc0a3da4dd5451494be007b32d4653ac
enonibobble <laidbackenon@outlook.com>
2025-11-28 06:56:20 +0000

    biyearly package updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,13 @@ export default {
   projectName: 'govigedit-docs-new', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'throw',
+      onBrokenMarkdownImages: 'throw'
+    }
+  },
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
